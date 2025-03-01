7e57fb13ab6d8dbc16d01cb10ff8b52f932ebdff
Steve Yee <steveplaysaxe@hotmail.com>
2025-03-01 10:50:35 -0800

    fix vite config for sub routes

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import solid from "vite-plugin-solid";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/theSlantedRoomSolid/",
   plugins: [solid()],
   resolve: {
     conditions: ["development", "browser"],
