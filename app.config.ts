0fd27b893d76b96c7a3489fe2318831bb06e70e3
Steve Yee <steveplaysaxe@hotmail.com>
2025-03-01 11:01:09 -0800

    cleanup

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "@solidjs/start/config";
-
 export default defineConfig({
   ssr: true, // Enable server-side rendering
   server: {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import WebDev from "~/components/WebDev/WebDev";
-import ParticleBanner from "~/components/ParticleBanner/ParticleBanner";
 import AboutMe from "~/components/AboutMe/AboutMe";
 import Composer from "~/components/Composer/Composer";
 import "./index.css";
@@ -26,10 +25,8 @@ export default function Home() {
         href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&family=PT+Sans+Narrow:wght@400;700&display=swap"
         rel="stylesheet"
       ></link>
-      {/* <ParticleBanner /> */}
       <AboutMe />
       <WebDev />
-      {/* <SoundEngineer /> */}
       <Composer />
     </main>
   );
